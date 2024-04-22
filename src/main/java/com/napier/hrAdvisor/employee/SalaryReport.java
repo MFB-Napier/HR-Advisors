20291986fa78786e915d9bb4ef72fb0a165b4787
MFB-Napier <40620574@live.napier.ac.uk>
2024-04-22 02:14:13 +0200

    upload Java class feature Produce a Salary Report

---
+++ src/main/java/com/napier/hrAdvisor/employee/SalaryReport.java
@@ -0,0 +1,9 @@
+package com.napier.hrAdvisor.employee;
+
+public class SalaryReport {
+
+    public static void main(String[] args)
+    {
+        System.out.println("Produce a Salary Report");
+    }
+}
